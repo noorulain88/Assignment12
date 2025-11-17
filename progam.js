f985158c30038f16a68d92f5f4d00579898b6752
noorulain88 <noorulainain88@gmail.com>
2025-11-17 20:40:26 +0500

    feat program

---
+++ progam.js
@@ -0,0 +1,59 @@
+// console.log("HELLO WORLD!");
+
+// // program 01
+// for (var i = 0; i < 100; i++) {
+//     console.log("The numbers from 0 to 100 ")
+// }
+
+// program 02
+// var i = 0
+// while (i < 50) {
+
+
+//     if (i % 2 == 0) {
+//         console.log("i", i)
+//     }
+
+
+//     i++
+// }
+
+// program 03
+// var i = 0;
+// do {
+//     if (i % 2 !== 0) {
+//         console.log("" ,i)
+//     }
+//     i++
+// } while (i < 100)
+
+
+// program 04
+
+// for (var i = 0; i < 100; i++) {
+// if( i % 5==0){
+//     console.log("the multiplication of 5",i)
+// }
+    
+// }
+
+// program 05
+
+
+// var firstNum=prompt("enter first number")
+// var secondeNum=prompt("enter seconde number")
+// var sum=secondeNum+firstNum
+//  for(var i=0;i<20;i++){
+
+//         console.log("sum", sum)
+  
+
+//  }
+
+// program 06
+// var i=50
+// while( i>0){
+//     console.log("the " ,i)
+//     i--
+// }
+// wrong
