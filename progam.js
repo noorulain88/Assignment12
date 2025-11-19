9c3620b9b58faaf560a767d0b233c99e0a5a127e
noorulain88 <noorulainain88@gmail.com>
2025-11-19 23:40:05 +0500

    feat:assignment 12

--- progam.js
+++ progam.js
@@ -1,11 +1,15 @@
 // console.log("HELLO WORLD!");
 
 // // program 01
+
+
 // for (var i = 0; i < 100; i++) {
 //     console.log("The numbers from 0 to 100 ")
 // }
 
 // program 02
+
+
 // var i = 0
 // while (i < 50) {
 
@@ -19,6 +23,8 @@
 // }
 
 // program 03
+
+
 // var i = 0;
 // do {
 //     if (i % 2 !== 0) {
@@ -37,7 +43,20 @@
     
 // }
 
+
 // program 05
+// var num=50;
+// while(num>0){
+//     console.log("decrement 50 to 1 " ,num);
+//     num--;
+// }
+
+
+
+
+
+
+// program 06
 
 
 // var firstNum=prompt("enter first number")
@@ -50,10 +69,159 @@
 
 //  }
 
-// program 06
+// program 07
+
 // var i=50
 // while( i>0){
 //     console.log("the " ,i)
 //     i--
 // }
-// wrong
+
+
+// program 08
+
+// var i=0
+
+// while(i<100){
+//     if(i % 3==0){
+//         console.log("the numbers divisible by 3" ,i)
+//     }
+//     i++
+// }
+
+// program 09
+
+// for (var i =10 ; i < 200; i++) {
+// if( i % 10==0){
+//     console.log("the multiplication of 10",i)
+// }
+    
+// }
+
+// program 10
+
+// check
+
+// var i=prompt("e")
+//  do {
+//     if (i * i) {
+//         console.log("" ,i )
+//     }
+//     i++
+// } while (i < 15)
+
+
+// program 11
+
+// var number=6
+// for(var i=0;i<10;i++){
+//     if(i==0){
+//         console.log("the factorial of is not 0 ")
+//     }
+//         if(i==1){
+//         console.log("the factorial of is 1")
+//     }
+// }
+
+
+// program 12
+// var fruits=['mango','banana','grapes','apple','orange']
+// fruits.push('coconet')
+// console.log("fruits name" ,fruits)
+
+
+// program 13
+
+// var fruits=['mango','banana','grapes','apple','orange']
+// fruits.pop()
+// console.log("fruits name" ,fruits)
+
+// program 14
+
+// var fruits=['black','blue','grey','red','green']
+// fruits.shift('coconet')
+// console.log("colors name in shift" ,fruits)
+
+// program 15
+
+// var fruits=['black','blue','grey','red','green']
+// fruits.unshift('yellow')
+// console.log("colors name in unshift" ,fruits)
+
+// program 16
+// var numbrers=['1','2','3','4','5' ,'6','7','8' ,'9','10']
+
+// console.log("numbers name" ,numbrers.slice(0,5) )
+// console.log("numbers name" ,numbrers)
+
+
+// program 17
+// var numbrers=['1','2','3','4','5' ,'6','7','8']
+// console.log("numbers name" ,numbrers.splice(4,2))
+// console.log("numbers name" ,numbrers)
+
+
+// program 18
+//  var colors=['black','blue','grey','red','green']
+//  colors.slice();
+//  colors.splice(2,0,'white','pink')
+//  console.log("colors name" ,colors)
+
+// program 19
+// var food=['biryani','burger','pizza','pasta','noodles']
+// food.splice(1,3,
+//     'chicken','fish','rice'
+// )
+// console.log("food items" ,food)
+
+
+
+// program 20
+
+// var animals=['dog','cat','tiger','lion','elephant']
+// animals.splice(2,2);
+// animals.splice(2,'tiger','lion')
+
+// console.log("animals name" ,animals)
+
+
+// program 21
+// var numbers=['1','2','3','4','5' ,'6','7','8' ,'9','10']
+
+// var newnumbers=numbers.slice(0,7)
+// console.log("numbers name" ,newnumbers )
+// console.log("numbers name" ,numbers)
+
+// dought
+
+// program 22
+
+// var scores=['10','20','30','40','50','60']
+// scores.splice(0,1);
+// console.log("scores" ,scores)
+
+// program 23
+//  var element=[ 'keybaord','mouse','moniter','cpu','speakers']
+
+// var newelement=element.slice(1,4)
+// console.log("element" ,newelement)
+// console.log("element" ,element)
+// dought
+
+// progrma 24
+// var array=[]
+// for( var i=0; i<5;i++){
+//    array.push(i)
+// }
+//  console.log("array" ,array)
+
+// program 25
+
+// var array=[]
+// while(array.length>5){
+//    array.pop()
+//    i++
+// }
+//  console.log("array" ,array)
+// asked
+
